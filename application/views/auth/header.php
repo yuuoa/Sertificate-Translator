a9135e89ddceaea465ea6df2292b665367845a8f
Alwan Andika <47649979+yuuoa@users.noreply.github.com>
2019-05-29 20:23:01 +0700

    V1.6

--- application/views/auth/header.php
+++ application/views/auth/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?= $title; ?> - Login</title>
+    <title>FST Penerjemahan Ijazah - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
